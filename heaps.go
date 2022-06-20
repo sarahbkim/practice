b4a3f1067c3bd37f1d9e2803de88a8f7dc58f7b1
sarah kim <3812995+sarahbkim@users.noreply.github.com>
2022-06-19 21:00:27 -0700

    mtg rooms

--- heaps.go
+++ heaps.go
@@ -1,6 +1,9 @@
 package main
 
-import "container/heap"
+import (
+	"container/heap"
+	"sort"
+)
 
 // TODO: can i use binary search? the inputs are sorted...
 func kthSmallest(matrix [][]int, k int) int {
@@ -59,4 +62,61 @@ func (h *MaxHeap) Pop() interface{} {
 	x := old[n-1]
 	*h = old[0 : n-1]
 	return x
+}
+
+func minMeetingRooms(intervals [][]int) int {
+	sort.Slice(intervals, func(a, b int) bool {
+		return intervals[a][0] < intervals[b][0]
+	})
+	var pq rooms
+	heap.Init(&pq)
+	for i := 0; i < len(intervals); i++ {
+		mtg := intervals[i]
+		if pq.Len() == 0 {
+			heap.Push(&pq, &room{value: mtg, meetingEnd: mtg[1]})
+		} else {
+			earliestMtgRoom := peek(&pq).(*room)
+			if earliestMtgRoom.meetingEnd < mtg[0] {
+				heap.Pop(&pq)
+				heap.Push(&pq, &room{value: mtg, meetingEnd: mtg[1]})
+			}
+
+		}
+	}
+	return pq.Len()
+}
+
+type room struct {
+	value      []int
+	meetingEnd int
+}
+
+type rooms []*room
+
+func (r rooms) Len() int { return len(r) }
+func (r rooms) Less(i, j int) bool {
+	return r[i].meetingEnd < r[j].meetingEnd
+}
+func (r rooms) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
+
+func (r *rooms) Push(x interface{}) {
+	item := x.(*room)
+	*r = append(*r, item)
+}
+func peek(r *rooms) interface{} {
+	old := *r
+	n := len(old)
+	item := old[n-1]
+
+	return item
+}
+func (r *rooms) Pop() interface{} {
+	old := *r
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	*r = old[0 : n-1]
+	return item
 }
